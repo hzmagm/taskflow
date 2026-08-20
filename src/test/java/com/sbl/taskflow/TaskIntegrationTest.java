450263372df180c23d08287583d83bc5320d59f2
hzmagm <hamzasibella@gmail.com>
2026-08-20 19:25:15 +0200

    feat: switch database to MySQL with Testcontainers support

--- src/test/java/com/sbl/taskflow/TaskIntegrationTest.java
+++ src/test/java/com/sbl/taskflow/TaskIntegrationTest.java
@@ -8,6 +8,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.webmvc.test.autoconfigure.AutoConfigureMockMvc;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+import org.testcontainers.junit.jupiter.Testcontainers;
 
 import static org.hamcrest.Matchers.hasSize;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
@@ -15,9 +16,18 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+import org.testcontainers.junit.jupiter.Container;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.testcontainers.mysql.MySQLContainer;
+
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @AutoConfigureMockMvc
+@Testcontainers
 class TaskIntegrationTest {
+
+    @Container
+    @ServiceConnection
+    static MySQLContainer mysql = new MySQLContainer("mysql:8.0");
 
     @Autowired
     MockMvc mockMvc;
@@ -31,7 +41,6 @@ class TaskIntegrationTest {
     }
 
 
-
     @Test
     void shouldCreateTaskSuccessfully() throws Exception {
         // Simulate the JSON payload from a frontend or postman
@@ -43,13 +52,7 @@ class TaskIntegrationTest {
                 }
                 """;
 
-        mockMvc.perform(post("/api/tasks")
-                        .contentType(MediaType.APPLICATION_JSON)
-                        .content(taskJson))
-                .andExpect(status().isCreated())
-                .andExpect(jsonPath("$.id").exists())
-                .andExpect(jsonPath("$.title").value("Nail the interview"))
-                .andExpect(jsonPath("$.status").value("PENDING"));
+        mockMvc.perform(post("/api/tasks").contentType(MediaType.APPLICATION_JSON).content(taskJson)).andExpect(status().isCreated()).andExpect(jsonPath("$.id").exists()).andExpect(jsonPath("$.title").value("Nail the interview")).andExpect(jsonPath("$.status").value("PENDING"));
     }
 
     @Test
@@ -61,13 +64,7 @@ class TaskIntegrationTest {
                     "status": "PENDING"\s
                 }
                 """;
-        mockMvc.perform(post("/api/tasks")
-                .contentType(MediaType.APPLICATION_JSON)
-                .content(taskJson))
-                .andExpect(status().isCreated());
-        mockMvc.perform(get("/api/tasks"))
-                .andExpect(status().isOk())
-                .andExpect(jsonPath("$", hasSize(1)))
-                .andExpect(jsonPath("$[0].title").value("Review Java 21 features"));
+        mockMvc.perform(post("/api/tasks").contentType(MediaType.APPLICATION_JSON).content(taskJson)).andExpect(status().isCreated());
+        mockMvc.perform(get("/api/tasks")).andExpect(status().isOk()).andExpect(jsonPath("$", hasSize(1))).andExpect(jsonPath("$[0].title").value("Review Java 21 features"));
     }
 }
